086d994b3e5e285f0836ef39662472131ba0d484
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-29 14:50:58 +0100

    commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let MenuBtn = document.querySelector(".menu-btn");
 let OpenMenu = document.querySelector(".open-menu");
 let MenuEl = document.querySelector(".menu");
+let BodyBlur = document.querySelector(".blur");
 
 MenuBtn.addEventListener("click", function () {
     OpenMenu.classList.remove("hidden");
     MenuEl.classList.remove("max-lg:hidden");
+    BodyBlur.classList.remove("hidden");
 });
 
 OpenMenu.addEventListener("click", function () {
     OpenMenu.classList.toggle("hidden");
     MenuEl.classList.toggle("max-lg:hidden");
+    BodyBlur.classList.add("hidden");
 });
